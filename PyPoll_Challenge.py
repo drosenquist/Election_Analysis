8b880fd0a366e5f469476414304b30028a1e9c48
drosenquist <101379969+drosenquist@users.noreply.github.com>
2022-04-10 18:14:26 -0700

    Updating files.

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -1,10 +1,9 @@
 # -*- coding: UTF-8 -*-
-"""PyPoll Homework Challenge Solution."""
-#%%
+"""PyPoll Homework Challenge Solution."""=
 # Add our dependencies.
 import csv
 import os
-#%%
+
 # Add a variable to load a file from a path.
 file_to_load = 'Resources/election_results.csv'
 # Add a variable to save the file to a path.
@@ -78,7 +77,6 @@ with open(file_to_load) as election_data:
         # 5: Add a vote to that county's vote count.
         county_turnout[county_name] += 1
 #print(county_votes)
-#%%
 # Save the results to our text file.
 with open(file_to_save, "w") as txt_file:
 
